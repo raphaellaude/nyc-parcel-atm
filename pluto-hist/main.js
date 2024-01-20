e8176e4ff1fd3585c806aa72e853e33de22d05fe
Raphael Paul Laude <raphlaude@gmail.com>
2024-01-20 11:30:02 -0500

    correctly render line layer

--- pluto-hist/main.js
+++ pluto-hist/main.js
@@ -40,51 +40,51 @@ map.on("load", function () {
       url: layerData.url,
     });
 
-    // map.addLayer({
-    //   id: layerData.id,
-    //   source: `pluto-${y}`,
-    //   "source-layer": layerData.id,
-    //   type: "fill",
-    //   layout: {
-    //     visibility: isVisible,
-    //   },
-    //   paint: {
-    //     "fill-color": [
-    //       "match",
-    //       ["get", layerData.columns[0].id],
-    //       "01", // 1 & 2 Family Buildings
-    //       "#feffa8",
-    //       "02", // Multi-Family Walk-Up Buildings
-    //       "#fcb841",
-    //       "03", // Multi-Family Elevator Buildings
-    //       "#c98e0e",
-    //       "04", // Mixed Residential & Commercial Buildings
-    //       "#ff8341",
-    //       "05", // Commercial & Office Buildings
-    //       "#cc3e3d",
-    //       "06", // Industrial & Manufacturing
-    //       "#c26dd1",
-    //       "07", // Transportation & Utility
-    //       "#dfbeeb",
-    //       "08", // Public Facilities & Institutions
-    //       "#519dc4",
-    //       "09", // Open Space & Outdoor Recreation
-    //       "#699466",
-    //       "10", // Parking Facilities
-    //       "#bab8b6",
-    //       "11", // Vacant Land
-    //       "#555555",
-    //       "#e7e7e7", // Other
-    //     ],
-    //   },
-    //   minzoom: 2,
-    //   maxzoom: 16,
-    // });
+    map.addLayer({
+      id: layerData.id,
+      source: `pluto-${y}`,
+      "source-layer": layerData.id,
+      type: "fill",
+      layout: {
+        visibility: isVisible,
+      },
+      paint: {
+        "fill-color": [
+          "match",
+          ["get", layerData.columns[0].id],
+          "01", // 1 & 2 Family Buildings
+          "#feffa8",
+          "02", // Multi-Family Walk-Up Buildings
+          "#fcb841",
+          "03", // Multi-Family Elevator Buildings
+          "#c98e0e",
+          "04", // Mixed Residential & Commercial Buildings
+          "#ff8341",
+          "05", // Commercial & Office Buildings
+          "#cc3e3d",
+          "06", // Industrial & Manufacturing
+          "#c26dd1",
+          "07", // Transportation & Utility
+          "#dfbeeb",
+          "08", // Public Facilities & Institutions
+          "#519dc4",
+          "09", // Open Space & Outdoor Recreation
+          "#699466",
+          "10", // Parking Facilities
+          "#bab8b6",
+          "11", // Vacant Land
+          "#555555",
+          "#e7e7e7", // Other
+        ],
+      },
+      minzoom: 2,
+      maxzoom: 16,
+    });
 
     map.addLayer({
       id: `${layerData.id}-line`,
       source: `pluto-${y}`,
-      "source-layer": `${layerData.id}-line`,
+      "source-layer": layerData.id,
       type: "line",
       layout: {
         visibility: isVisible,
@@ -94,10 +94,14 @@ map.on("load", function () {
         "line-width": ["interpolate", ["linear"], ["zoom"], 11, 0.25, 16, 0.5],
         "line-opacity": ["interpolate", ["linear"], ["zoom"], 11, 0, 16, 1],
       },
+      minzoom: 2,
+      maxzoom: 16,
     });
   });
 
   getZoom(map);
+
+  console.log(map.getStyle().layers);
 });
 
 map.on("zoom", function () {
@@ -133,7 +137,7 @@ document.onkeydown = function (e) {
             "visibility",
             "none"
           );
-        }, 750);
+        }, 750); // if at larger zoom levels, wait longer
       }
       break;
     case "s":
@@ -153,7 +157,7 @@ document.onkeydown = function (e) {
             "visibility",
             "none"
           );
-        }, 750);
+        }, 750); // if at larger zoom levels, wait longer
       }
       break;
     case "q":
