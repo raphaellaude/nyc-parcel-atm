f7c1092d4f725c2a83ae149a22185c477c910b65
Raphael Paul Laude <raphlaude@gmail.com>
2024-01-28 16:38:34 -0500

    single year endpoint

--- backend/data/converters.py
+++ backend/data/converters.py
@@ -1,5 +1,9 @@
+LAT_REGEX = r'^(\+|-)?(?:90(?:(?:\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,14})?))$'
+LON_REGEX = r'^(\+|-)?(?:180(?:(?:\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,14})?))$'
+YEARS_REGEX = r"\b(?:0[1-9]|1\d|2[0-3])\b"
+
 class LatConverter:
-    regex = r'^-?(?:90(?:(?:\.0{1,3})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,6})?))$'
+    regex = LAT_REGEX
 
     def to_python(self, value):
         print(value)
@@ -11,7 +15,7 @@ class LatConverter:
 
 
 class LonConverter:
-    regex = r'^-?(?:180(?:(?:\.0{1,3})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,6})?))$'
+    regex = LON_REGEX
 
     def to_python(self, value):
         print(value)
--- backend/data/views.py
+++ backend/data/views.py
@@ -1,5 +1,31 @@
+import re
 from django.http import HttpResponse
+from django.db import connection
+from data.converters import LAT_REGEX, LON_REGEX, YEARS_REGEX
+from data.jinja import render_template
 
-# Create your views here.
-def single_year_pluto(request, year, lat, lon):
-    return HttpResponse(f"Single year pluto {year} {lat} {lon}")
+
+def single_year_pluto(_, year, lon, lat):
+    """
+    Single year pluto view. 
+    """
+    if not re.match(LON_REGEX, lon):
+        return HttpResponse("Invalid longitude", status=400)
+
+    if not re.match(LAT_REGEX, lat):
+        return HttpResponse("Invalid latitude", status=400)
+
+    if not re.match(YEARS_REGEX, year):
+        return HttpResponse("Invalid year", status=400)
+
+    sql = render_template("sql/spatial_join.sql.jinja", table=f"data_pluto{year}")
+
+    cursor = connection.cursor()
+    result = cursor.execute(sql, (lon, lat,))
+    result = cursor.fetchall()
+
+    cols = [d[0] for d in cursor.description]
+
+    html = render_template("html/record_table.html.jinja", record=zip(cols, result[0]))
+
+    return HttpResponse(html)
