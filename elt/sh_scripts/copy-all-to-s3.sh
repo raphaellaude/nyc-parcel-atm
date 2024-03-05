6f19423c70370f7699ac98f11cccf90b0d6a85c4
Raphael Paul Laude <raphlaude@gmail.com>
2024-03-04 23:15:03 -0500

    remove old pipeline sh scripts

--- elt/sh_scripts/copy-all-to-s3.sh
+++
@@ -1,5 +0,0 @@
-for f in /Users/raphaellaude/Documents/Projects/dxd/processed_data/tiles/*.pmtiles; do
-    filename=$(basename -- $f)
-    echo $filename
-    aws s3 cp $f "s3://pluto-hist/data/$filename"
-done
--- elt/sh_scripts/zip-big-files.sh
+++
@@ -1,5 +0,0 @@
-for f in /Users/raphaellaude/Documents/Projects/dxd/processed_data/unioned/*.geojson; do
-  filename=$(basename -- "$f")
-  filename="${filename%.*}"
-  zip $filename $f
-done
